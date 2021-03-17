1f1326005ea76334346bdaf7a763b7f6f8d63450
Maciek Kacz <maciek.kacz@wp.pl>
2021-03-17 21:22:30 +0100

    defining an entity

---
+++ src/main/java/devicerent/DevicrentApp.java
@@ -0,0 +1,12 @@
+package devicerent;
+
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DevicrentApp {
+    public static void main(String[] args) {
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("devicrentPU");
+        emFactory.close();
+    }
+}
